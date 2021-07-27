8edc3b91e675014e3ba4d35f04858660a125e8b6
César Lata <cesarlata1@hotmail.com>
2021-07-26 22:10:06 -0500

    second commit

--- js/animes.js
+++ js/animes.js
@@ -1,14 +1,37 @@
-import { getAnimes } from "./endpoints.js";
+import { getAnimes, getAnime, getGenres } from "./endpoints.js";
 import { drawCard } from "./card-animes.js";
 
 let search = document.getElementById('search')
 
 search.onkeyup = async function() {
 
-    if(search.value != '') {
+    if(search.value.length >= 4) {
         const animes = await getAnimes(search.value)
-        let data = await animes.json()
-        drawCard(data.results)
+        
+        let id = animes.results[0].mal_id
+
+        let anime = await getAnime(id)
+
+        let genres = anime.genres.slice(0, 2).map( element => element.mal_id);
+        
+        Promise.all([getGenres(genres[0]), getGenres(genres[1])]).then( suggestedAnime => {
+
+            let animeRule = (animes, gendre)  => animes.concat(gendre.anime.slice(0, 3));
+
+            let arrayOfAnime = suggestedAnime.reduce(animeRule, [])
+    
+            let completeAnimes = Promise.all(arrayOfAnime.map( anime => getAnime(anime.mal_id) ))
+    
+            //limpiar contenedor
+
+            // completeAnimes.forEach( animePromise => setTimeout( async () => { 
+            //     drawOneCard( await animePromise ) 
+            // }, 0 ) )
+
+        })
+        
+        drawCard(animes.results)
+
     }
 
 }
--- js/endpoints.js
+++ js/endpoints.js
@@ -1,6 +1,14 @@
 export const getAnimes = async (search = '', page = 1) => {
     let animes = await fetch(`https://api.jikan.moe/v3/search/anime?q=${search}&page=${page}&limit=6`)    
-    return animes
+    return animes.json()
 }
 
+export const getAnime = async (id) => {
+    let anime = await fetch(`https://api.jikan.moe/v3/anime/${id}`)
+    return anime.json()
+}
 
+export const getGenres = async (genre_id) => {
+    let genres = await fetch(`https://api.jikan.moe/v3/genre/anime/${genre_id}`)
+    return genres.json()
+}
